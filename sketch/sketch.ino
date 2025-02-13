a92c8108f3d21ac54dec5d47c10296c6a3f1c2b2
wonggodbird <wonggodbird@gmail.com>
2025-02-13 16:29:51 +0700

    Update sketch.ino
    add sensor no.2

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -24,52 +24,100 @@
 #define NOTE_A4  440 // A4
 //#define NOTE_D5  587 // D5
 
-// Ultrasonic sensor
-#define TRIGGER_PIN  6  // Pin trigger sensor
-#define ECHO_PIN     5  // Pin echo sensor
+// Ultrasonic sensor 1
+#define TRIGGER_PIN1  6  // Pin trigger sensor 1
+#define ECHO_PIN1     5  // Pin echo sensor 1
 
-#define MAX_DISTANCE 500 // Maximum distance we want to ping for (in centimeters).
+// Ultrasonic sensor 2
+#define TRIGGER_PIN2  8  // Pin trigger sensor 2
+#define ECHO_PIN2     7  // Pin echo sensor 2
+
+#define MAX_DISTANCE1 500 // Maximum distance we want to ping for (in centimeters).
+#define MAX_DISTANCE2 500 
 
 // กำหนดพินสำหรับการใช้งานเสียง
-const int buzzerPin = 9;  // พินสำหรับเสียง sensor
-unsigned int distance;  // ตัวแปรสำหรับเก็บค่าระยะ sensor
-int LED = 3;  //LED status
+const int buzzerPin1 = 9;  // พินสำหรับเสียง sensor 1
+const int buzzerPin2 = 10;  // พินสำหรับเสียง sensor 2
 
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // Sensor setup
+unsigned int distance1;  // ตัวแปรสำหรับเก็บค่าระยะ sensor 1
+unsigned int distance2;  // ตัวแปรสำหรับเก็บค่าระยะ sensor 2
+
+int LED_1 = 3;  //LED status
+int LED_2 = 4;
+
+NewPing sonar1(TRIGGER_PIN1, ECHO_PIN1, MAX_DISTANCE1); // Sensor 1 setup
+NewPing sonar2(TRIGGER_PIN2, ECHO_PIN2, MAX_DISTANCE2); // Sensor 2 setup
 
 void setup() 
 {
   Serial.begin(9600); 
-  pinMode(buzzerPin, OUTPUT); // กำหนดพิน output สำหรับเสียง sensor
-  pinMode(LED, OUTPUT);
+  pinMode(buzzerPin1, OUTPUT); // กำหนดพิน output สำหรับเสียง sensor 1
+  pinMode(buzzerPin2, OUTPUT); // กำหนดพิน output สำหรับเสียง sensor 2
+
+  pinMode(LED_1, OUTPUT);
+  pinMode(LED_2, OUTPUT);
 }
 
 void loop() 
 {
   delay(10);  // Wait 10ms between pings
 
-  // อ่านค่าระยะจาก sensor
-  distance = sonar.ping_cm(); 
-  Serial.print("Sensor : ");
-  Serial.print(distance);
+  // อ่านค่าระยะจาก sensor 1
+  distance1 = sonar1.ping_cm(); 
+  Serial.print("Sensor 1: ");
+  Serial.print(distance1);
   Serial.print(" cm");
 
-  // การทำงานของ sensor
-  if (distance > 20 && distance <= 100  ) // ระยะ 20 - 100 cm
+  // อ่านค่าระยะจาก sensor 2
+  distance2 = sonar2.ping_cm();
+  Serial.print("  |  Sensor 2: ");
+  Serial.print(distance2);
+  Serial.println(" cm");
+
+  // การทำงานของ sensor 1
+  if (distance1 > 20 && distance1 <= 100  ) // ระยะ 20 - 100 cm
   {
-    digitalWrite(LED, LOW);  // LED ON
-    playTone(buzzerPin, NOTE_E4, distance); //ok 
-    Serial.println("playTone"); 
+    digitalWrite(LED_1, LOW);  // LED ON
+    playTone1(buzzerPin1, NOTE_E4, distance1); //ok 
+    Serial.println("                                         playTone1"); 
   } 
   else 
   {
-    noToneManual(buzzerPin); // หยุดเสียง sensor
-    digitalWrite(LED, HIGH);  //  LED OFF
+    noToneManual(buzzerPin1); // หยุดเสียง sensor 1
+    digitalWrite(LED_1, HIGH);  //  LED OFF
+  }
+
+  // การทำงานของ sensor 2
+  if (distance2 > 20 && distance2 <= 100) // ระยะ 20 - 100 cm
+  {
+    digitalWrite(LED_2, LOW);  // LED ON
+    playTone2(buzzerPin2, NOTE_C5, distance2); // เล่นเสียง C4 จาก    //ดัง  11  ok
+    Serial.println("                                                          playTone2");
+  } 
+  else 
+  {
+    noToneManual(buzzerPin2); // หยุดเสียง sensor 2
+    digitalWrite(LED_2, HIGH);  // LED OFF
   }
 }
 
 // ฟังก์ชันสร้างเสียงแบบกำหนดเอง
-void playTone(int pin, int frequency, int duration) 
+void playTone1(int pin, int frequency, int duration) 
+{
+  int period = 1000000 / frequency; // คำนวณคาบเวลา (ไมโครวินาที)
+  int pulse = period / 2; // ความกว้างของพัลส์
+  unsigned long startTime = millis();
+  while (millis() - startTime < duration) 
+  {
+    digitalWrite(pin, HIGH);
+    delayMicroseconds(pulse);
+    digitalWrite(pin, LOW);
+    delayMicroseconds(pulse);
+  }
+}
+
+// ฟังก์ชันสร้างเสียงแบบกำหนดเอง
+void playTone2(int pin, int frequency, int duration) 
 {
   int period = 1000000 / frequency; // คำนวณคาบเวลา (ไมโครวินาที)
   int pulse = period / 2; // ความกว้างของพัลส์
